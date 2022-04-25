48abc3e345059a45e2395ed0b58185400af92642
Jessica Pozo <jessicapozo198715@gmail.com>
2022-04-25 17:29:22 +0200

    subiendo aplicacion de mapas-extras

---
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -0,0 +1,43 @@
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { Map, Popup, Marker } from 'mapbox-gl';
+import { PlacesService } from '../../services';
+import { MapService } from '../../services/map.service';
+
+@Component({
+  selector: 'app-map-view',
+  templateUrl: './map-view.component.html',
+  styleUrls: ['./map-view.component.css']
+})
+export class MapViewComponent implements AfterViewInit {
+
+  @ViewChild('mapDiv') 
+  mapDivElement!: ElementRef
+
+  constructor( private placesService: PlacesService,
+    private mapService: MapService ) { }
+
+    ngAfterViewInit(): void {
+
+      if ( !this.placesService.useLocation ) throw Error('No hay placesService.userLocation');
+      
+      const mapa = new Map({
+        container: this.mapDivElement.nativeElement,
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: this.placesService.useLocation,
+        zoom: 14,
+      });
+
+      const popup = new Popup()
+      .setHTML(`
+        <h6>Aquí estoy</h6>
+        <span>Estoy en este lugar del mundo</span>
+      `);
+
+      new Marker({ color: 'red' })
+      .setLngLat( this.placesService.useLocation )
+      .setPopup( popup )
+      .addTo( mapa )
+
+      this.mapService.setMap( mapa );
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiamVzc2ljYXBvem8iLCJhIjoiY2wyZDFtOWloMHVjdTNjbm80dW5sa3hicCJ9.mYIcertw_njmvUb8A-_z3Q';
+
+
+if ( !navigator.geolocation ){
+  alert('Navegador no soporta el Geolocation');
+  throw new Error('Navegador no soporta el Geolocation');
+}
+
 if (environment.production) {
   enableProdMode();
 }
